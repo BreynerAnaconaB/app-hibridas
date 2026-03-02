2af89d2ea40458217446cbbff64a36d54a4f8cf7
Julian-cryp <julianogaraytorres@gmail.com>
2026-03-01 23:03:07 -0500

    index 1.0 , aun falta hover de imagenes y otras cosas

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,75 @@
 import { Text } from "@react-navigation/elements";
-import { FlatList, View } from "react-native";
+import { FlatList, ScrollView, TouchableOpacity, View } from "react-native";
 import estilos from "../css/estilos";
 
 
-interface Introduccion{
-  title:string,
-  text:string,
+interface Introduccion {
+  title: string,
+  text: string,
+   imagen?:string
+}
+
+interface Fotos_texto{
+  title: string,
+  imagen:string,
 }
 
 const task: Introduccion[] = [{
-  title:"Tecnano",
-  text: "hola pepito",
+  title: "BIENVENIDO A TECNANO",
+  text: "Elevamos el rendimiento de tu hardware al siguiente nivel. montaje de componentes y soluciones avanzadas de software en Bogotá.",
 }];
 
+const datosIndex: Fotos_texto[] = [{
+   title:"COMPONENTES",
+   imagen: "https://www.muycomputer.com/wp-content/uploads/2021/10/placa-base.jpg",
+   
+},
+{
+  title:"SERVICIO",
+   imagen: "https://www.muycomputer.com/wp-content/uploads/2021/10/placa-base.jpg",
+}
 
-function renderItem({item}:{item:Introduccion}){
+
+];
+
+
+function renderItem({ item }: { item: Introduccion }) {
   return (
-  <View>
-    <Text style={estilos.text}>{item.title}</Text>
-  </View>
+    <View style={estilos.Container}>
+      <Text style={[estilos.Tituloindex]}>{item.title}</Text>
+      <Text style={[estilos.textIndex]}>{item.text}</Text>
+    </View>
   );
 }
 
-export default function informacion(){
-  return(<View>
-   <FlatList<Introduccion>
-    data={task}
-    renderItem={renderItem}
-    keyExtractor={(item) => item.title}
-/>
-</View>)
-  
+function renderDetalles_producto({item}: { item: Fotos_texto}){
+  return(
+    <View style={estilos.ContainerInfo}>
+<TouchableOpacity>
+  <Text style={estilos.tituloTarjeta}>{item.title}</Text>
+
+</TouchableOpacity>
+</View>
+
+  );
+}
+
+export default function paginaPrincipal(){
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+  <View>
+    <FlatList <Introduccion>
+      data={task}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.title}
+    />
+      <FlatList<Fotos_texto>
+        data={datosIndex}
+        renderItem={renderDetalles_producto}
+        numColumns={2}
+        keyExtractor={(item) => item.title}
+          />
+    </View>
+    </ScrollView>
+  )
 }
--- app/css/estilos.tsx
+++ app/css/estilos.tsx
@@ -1,13 +1,13 @@
 import { StyleSheet } from "react-native";
- 
+
 const estilos = StyleSheet.create({
     Titulo: {
         fontSize: 20,
         color: 'blue',
         backgroundColor: 'gray',
-        display:'flex',
-        justifyContent:'center',
-        textAlign:'center',
+        display: 'flex',
+        justifyContent: 'center',
+        textAlign: 'center',
         padding: 10,
         borderRadius: 10
     },
@@ -16,42 +16,109 @@ const estilos = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 15,
         width: '50%',
-        padding: 10,  
+        padding: 10,
     },
     inputcontainer: {
         marginTop: 15,
         width: '100%',
         alignItems: 'center',
     },
-    withetext:{
-      fontSize:20,
-      color:'black',
+    withetext: {
+        fontSize: 20,
+        color: 'black',
     },
-    boton:{
-      marginTop: 15,
-      borderColor:'blue',
-      backgroundColor:'gray',
-      borderRadius:10,
-      padding:10
+    boton: {
+        marginTop: 15,
+        borderColor: 'blue',
+        backgroundColor: 'gray',
+        borderRadius: 10,
+        padding: 10
     },
-    textdone:{
-        fontSize:16,
-        textDecorationLine:"line-through"
+    textdone: {
+        fontSize: 16,
+        textDecorationLine: "line-through"
     },
-    text:{
-        fontSize:50,
-        color:"#000000",
-        display:'flex',
+    Tituloindex: {
+        fontFamily: 'sans-serif-condensed',
+        fontWeight: 800,
+        fontSize: 40,
+        borderRadius: '20%',
+        marginTop:'2%',
+        color: "#000000",
+        flexDirection:'row',
+        justifyContent: 'center',
+        padding: 20,
+        margin:30
+    },
+
+    textIndex: {
+        fontSize: 26,
+        marginLeft:20,
+        fontFamily: 'sans-serif-medium',
+        fontWeight: 400,
+        color: '#0d0d0e',
+        maxWidth: 500,
+        marginTop: 0,
+        padding: 30,
+
+
+    },
+    estilosProducto: {
+        width: 200,
+        height: 200
+
+    },
+    ContainerImg: {
+      width:400,
+      height:300,
+      resizeMode:'contain'
+    },
+    Container: {
+          backgroundColor: '#ffffff',
+        borderRadius: 10,
+        padding: 10,
+        margin: 27,
+        flex: 1,
+    
         justifyContent:'center',
-        padding:40
+        alignItems:'baseline',
+
+        shadowColor: '#a0a0a0',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.05,
+        shadowRadius: 10,
+        elevation: 2,
     },
-    eliminar:{
-        backgroundColor:"red"
+
+    ContainerInfo:{
+        
+            backgroundColor: '#8f8f8f',
+            width:200,
+            height:400,
+           borderRadius: 10,
+           padding: '10%',
+           margin: '8%',
+           marginTop:'2%',
+
+            flex: 2,
+        justifyContent:"center",
+        alignItems: 'center',
+
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.05,
+        shadowRadius: 10,
+        elevation: 2,
     },
-    estilosProducto:{
-        width:200,
-        height:200
+
+    tituloTarjeta: {
+        fontSize: 30,
+        fontWeight: '700',
+        color: '#ffffff',
+        marginBottom: 8,
     }
+
+
 });
 
 export default estilos;
