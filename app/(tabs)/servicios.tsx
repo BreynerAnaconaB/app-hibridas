b0e45d04838b9ea3bbe7bcbd8abe097374ae6bbe
JuanPonce333 <pipeponce334@gmail.com>
2026-05-05 13:33:31 -0500

    prueba

---
+++ app/(tabs)/servicios.tsx
@@ -0,0 +1,15 @@
+
+interface Product {
+    id: string
+    nombre: string
+    precio: string
+    categoria: string
+}
+
+const servicios = [
+  { id: "1", nombre: "Reparación de celulares", descripcion: "Pantallas, baterías y más" },
+  { id: "2", nombre: "Mantenimiento de PC", descripcion: "Limpieza y optimización" },
+  { id: "3", nombre: "Instalación de software", descripcion: "Programas y sistemas operativos" },
+  { id: "4", nombre: "Soporte técnico", descripcion: "Asistencia presencial o remota" },
+  { id: "5", nombre: "Venta de accesorios", descripcion: "Cargadores, audífonos, etc." },
+]
